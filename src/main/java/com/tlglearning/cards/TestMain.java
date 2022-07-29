e3074983e9fea4c0aec81f931819e98fa1554d01
scaffidic <cp.scaffidi@gmail.com>
2022-07-29 13:17:39 -0700

    added comparator to sort first by rank, then by suit

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -2,6 +2,7 @@ package com.tlglearning.cards;
 
 import com.tlglearning.cards.model.Card;
 import com.tlglearning.cards.model.Deck;
+import com.tlglearning.cards.strategy.RankFirstComparator;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -11,11 +12,18 @@ public class TestMain {
     Deck deck = new Deck();
     Random rng = new SecureRandom();
     System.out.println(deck);
+
     deck.shuffle(rng);
     System.out.println(deck);
-    for (Card card : deck) {
-      System.out.println(card);
-    }
+//    for (Card card : deck) {
+//      System.out.println(card);
+//    }
+
+    deck.sort();
+    System.out.println(deck);
+
+    deck.sort(new RankFirstComparator());
+    System.out.println(deck);
   }
 
 }
